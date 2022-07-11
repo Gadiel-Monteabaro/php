4f7c0ee2dc90fa0309b5395423d1c57930793a4c
Letoublon96 <silvagadiel96@gmail.com>
2022-07-11 18:08:52 -0300

    cambios

--- listado_productos_while.php
+++ listado_productos_while.php
@@ -40,7 +40,7 @@ $aProductos[] = array(
 </head>
 
 <body>
-    <main class="container">
+    <main>
         <div class="container">
             <div class="row">
                 <div class="col-12 py-5 text-center">
