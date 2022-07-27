29811d9bd2688253010fa7c659ae636d5b880624
Gadiel-Monteabaro <silvagadiel96@gmail.com>
2022-07-27 05:58:25 -0300

    cambios

--- contar.php
+++ contar.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $aNotas = array(9, 12, 1, 1, 5, 4, 7, 4, 7, 4, 8, 4, 95, 0);
 
 //definicion 
--- neto.php
+++ neto.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 //definicion
 function calcularNeto($bruto){
 echo $neto = $bruto - ($bruto * 0.17) ;
