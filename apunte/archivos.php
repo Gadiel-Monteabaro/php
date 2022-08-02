13188e195da75580867919832f0792d603abdf2f
Gadiel-Monteabaro <silvagadiel96@gmail.com>
2022-08-02 10:06:16 -0300

    cambios

--- apunte/archivos.php
+++ apunte/archivos.php
@@ -7,19 +7,46 @@ PHP - Archivos
 
 // fopen => Abre un archivo y devuelve un puntero al archivo.
 //Syntaxis
-$archivo1 = fopen($nombreArchivo, $modo);
+$nameFile = "datos.txt";
+$archivo1 = fopen($nameFile, $mode);
 /*
 Modos posibles
 r => abre un archivo solo en modo lectura
 r+ => abre un archivo para leer o escribir
 w => solo permite escribir y no leer
-w+ => permite leer o escribie un archivo, si no existe crea el archivo 
+w+ => permite leer o escribir un archivo, si no existe crea el archivo 
 a => agrega al final
 a+ => agrega al final, si no existe crea el archivo
 */
+// CUIDADO => No es lo mismo abrir el archivo en modo 'r' que en 'a'
 
 // fwrite => Escribe en el archivo
 fwrite($archivo1, '1'); // Escribe en el archivo '1'
 
 // fclese => Cierra el archivo
 fclose($archivo1); // Cierra el archivo
+
+// fread => Lectura de un archivo 
+$tamanio = filesize($nameFile); // Me devuelve el tamaño del archivo en bytes.
+$contenido = fread($archivo1, $tamanio); // Lee un archivo, hasta el tamaño especificado
+
+//file_get_contents => Levanta un archivo en cadena de texto
+$contenido = file_get_contents($nameFile); // Equivale a hacer un 'fopen', 'fread', 'fclose'
+
+//file_put_contents => Esta funcion sobreescribe los datos del archivo.
+$nombre = "Gadiel Silva";
+file_put_contents($nameFile, $nombre);
+
+
+/*
+Un archivo de texto pouede ser demasiado grande, entonces lo que hacemos es leerlo linea por linea, ya que puede provocar de memoria.
+*/
+// la escritura seria:
+$archivo = fopen($nameFile, 'r');
+if ($archivo) {
+    //leemos la linea
+    while ($fila = fgets($archivo) == true) {
+        echo $fila;
+    }
+}
+fclose($archivo);
