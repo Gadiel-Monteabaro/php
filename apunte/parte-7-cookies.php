edb7852327e5904d827faf375ce0a3db24ad5256
Gadiel-Monteabaro <silvagadiel96@gmail.com>
2022-08-24 16:49:37 -0300

    cambios

---
+++ apunte/parte-7-cookies.php
@@ -0,0 +1,18 @@
+<?php
+/* 
+Cookies => es informacion que envia el servidor web al cliente,
+Con informacion que queda guardad en la pc de cada usuario  
+El browser almacena esta informacion en texto plano de no mas de 2kb
+
+
+// ¿Para que utilizamos Cookies?
+
+utilizamos cookies propias y de terceros para elaborar informacion estadistica y para mostrarle publicidad, contenidos y servicios personalizados en base a la informacioon de tu navegacion. 
+Por otra parte se comparte los analisis de navegacion y los grupos de interes inferidos con las empresas colaboradoras con el fin de personalizar la publicidad.
+
+
+$_COOKIE => Es una variable global que solo es de lectura
+-la misma es una array asociativo que contendra informacion que se haya guardado previamente
+-Cada posicion contendra la informacion de una cookie, donde la "clave" de la posicion sera el nombre de la cookie.
+
+*/
