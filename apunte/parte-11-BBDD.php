a4530fb8e47b5a77105a7c2136baf2650a55342b
Gadiel-Monteabaro <silvagadiel96@gmail.com>
2022-08-29 14:24:10 -0300

    cambios

---
+++ apunte/parte-11-BBDD.php
@@ -0,0 +1,38 @@
+<?php
+/*
+¿Que es una Base de Datos?
+-Conjunto de Datos pertenecientes a un mismo contexto.
+-Representan aspectos de la realidad.
+-Organizados con un proposito general.
+
+¿Para que Sirve?
+-Almacenar,modificar,agregar,eliminar datos.
+-Acceder datos en el futuro.
+-Manipular los datos, combinarlos.
+-Analizar datos.
+
+Ejemplos de BBDD.
+Oracle-Database
+MySQL
+Mongo DB
+Maria DB
+PostgreSQL
+Microsoft access
+IBM-DB2
+
+Un sistema de base de datos nos permite interactuar con los datos almacenados en el disco rigido de una manera eficiente.
+
+-Los sistemas de base de datos que vamos a utilizar son MySQL/MariaDB.
+-Es un SGBD (Sistemas de Gestion de Base de Datos.)
+-Mas popular para entornos de paginas Web.
+-Eficiente.
+-Parte del Stack Web XAMP/LAMP.
+
+Caracteristicas
+-Multiplataforma => Funciona sobre sistemas UNIX, Windows, MAC, Entre Otros.
+-Estandar SQL92 => Para todas las bases de datos, tipos de datos, codificacion, json.
+-Open Source => Es gratuito, Adaptable a tus necesidades.
+-Relacional => Normalizacion. integridad referencial.
+-Velocidad => rapidez en transacciones.
+-Multiconexiones => Seguridad, ,Multi-Hilos, multiple usuarios.
+*/
