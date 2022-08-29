281f8fe90ebe729b88872e66250a9630c9d17fdf
Gadiel-Monteabaro <silvagadiel96@gmail.com>
2022-08-29 17:31:25 -0300

    cambios

---
+++ ejercicio-poo.php
@@ -0,0 +1,15 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+class Persona {
+    public $dni;
+    public $nombre;
+    public $edad;
+    public $nacionalidad;
+    public function imprimir()
+    {
+
+    }
+}
