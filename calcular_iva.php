7cbb87a1a4c6b0a247fd453ac96a18dd1b54794d
Gadiel-Monteabaro <silvagadiel96@gmail.com>
2022-08-30 15:17:21 -0300

    cambios

--- calcular_iva.php
+++ calcular_iva.php
@@ -45,7 +45,7 @@ if ($_POST) {
     <div class="container">
         <div class="row">
             <div class="col-12 text-center py-5">
-                <h1>Calculadora de IVA</h1>
+                <h1>Calculadora dse IVA</h1>
             </div>
         </div>
     </div>
