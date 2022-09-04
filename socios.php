96af8ef88dd6e2392fb4dcd1676593ec2108d567
Gadiel-Monteabaro <silvagadiel96@gmail.com>
2022-09-04 18:07:15 -0300

    cambiso

--- socios.php
+++ socios.php
@@ -13,14 +13,19 @@ class Tarjeta
     private $tipo;
     private $cvv;
 
-    public function __construct($tipo, $numero, $fechaEmision, $fechaVto, $cvv)
-    {
-    }
-
-
     const VISA = "VISA";
     const AMEX = "American Express";
-    const MASTERCARD = "MASTERCARD";
+    const MASTERCARD = "Mastercard";
+    const NARANJA = "Naranja";
+    const CABAL = "CABAL";
+
+    public function __construct($tipo, $numero, $fechaVto, $cvv)
+    {
+        $this->numero = $numero;
+        $this->fechaVto = $fechaVto;
+        $this->tipo = $tipo;
+        $this->cvv = $cvv;
+    }
 
     public function __set($name, $value)
     {
@@ -62,7 +67,7 @@ class Cliente extends Persona
     {
         $this->aTarjetas = array();
         $this->bActivo = true;
-        $this->fechaAlta = date("d/m/Y");
+        $this->fechaAlta = date("d/m/Y H:i:s");
     }
     public function __set($name, $value)
     {
@@ -76,12 +81,12 @@ class Cliente extends Persona
 
     public function  agregarTarjeta($tarjeta)
     {
-        $this->aTarjetas = $tarjeta;
+        $this->aTarjetas[] = $tarjeta;
     }
 
     public function darDeBaja($fecha)
     {
-        $this->fechaBaja = date_format(date_create($fecha), "d/m/a");
+        $this->fechaBaja = $fecha;
         $this->bActivo = false; //Baja Logica.
     }
 
@@ -97,6 +102,7 @@ $cliente1->dni = "35123789";
 $cliente1->nombre = "Ana Valle";
 $cliente1->correo = "ana@correo.com";
 $cliente1->celular = "+54 3518053537";
+$cliente1->darDeBaja("04/09/2022");
 
 $tarjeta1 = new Tarjeta(Tarjeta::VISA, "4223750778806383", "03/2022", "01/2023", "275");
 $tarjeta2 = new Tarjeta(Tarjeta::AMEX, "347572886751981", "05/2020", "07/2027", "136");
@@ -105,3 +111,15 @@ $tarjeta3 = new Tarjeta(Tarjeta::MASTERCARD, "5415620495970009", "06/2021", "12/
 $cliente1->agregarTarjeta($tarjeta1);
 $cliente1->agregarTarjeta($tarjeta2);
 $cliente1->agregarTarjeta($tarjeta3);
+
+
+$cliente2 = new Cliente();
+$cliente2->dni = "48456876";
+$cliente2->nombre = "Bernabe Paz";
+$cliente2->correo = "bernabe@correo.com";
+$cliente2->celular = "1145326787";
+$cliente2->agregarTarjeta(new Tarjeta(Tarjeta::VISA, "4969508071710316", "08/2025", "865"));
+$cliente2->agregarTarjeta(new Tarjeta(Tarjeta::MASTERCARD, "5149107669552238", "04/2025", "554"));
+
+print_r($cliente1);
+print_r($cliente2);
