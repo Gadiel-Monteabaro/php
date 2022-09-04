584313d89fcd54dabd912e51c61b57d2ed079d14
Gadiel-Monteabaro <silvagadiel96@gmail.com>
2022-09-04 17:00:32 -0300

    cambios

--- socios.php
+++ socios.php
@@ -2,3 +2,79 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+
+class Tarjeta
+{
+    private $nombreTitular;
+    private $numero;
+    private $fechaEmision;
+    private $fechaVto;
+    private $tipo;
+    private $cvv;
+
+    public function __set($name, $value)
+    {
+        return $this->$name = $value;
+    }
+
+    public function __get($name)
+    {
+        return $this->$name;
+    }
+}
+
+
+class Persona
+{
+    protected $dni;
+    protected $nombre;
+    protected $correo;
+    protected $celular;
+
+    public function __set($name, $value)
+    {
+        return $this->$name = $value;
+    }
+
+    public function __get($name)
+    {
+        return $this->$name;
+    }
+}
+
+class Cliente extends Persona
+{
+    private $aTarjetas;
+    private $bActivo;
+    private $fechaAlta;
+    private $fechaBaja;
+
+    public function __construct()
+    {
+        $this->aTarjetas = array();
+        $this->bActivo = true;
+        $this->fechaAlta = date("d/m/Y H:i:s");
+    }
+    public function __set($name, $value)
+    {
+        return $this->$name = $value;
+    }
+
+    public function __get($name)
+    {
+        return $this->$name;
+    }
+
+    public function  agregarTarjeta()
+    {
+    }
+
+    public function darDeBaja()
+    {
+    }
+
+    public function  imprimir()
+    {
+    }
+}
