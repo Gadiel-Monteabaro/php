71b2147ede1b862c88de4d98a11db23f8edcc4c9
Gadiel-Monteabaro <silvagadiel96@gmail.com>
2022-09-18 20:37:14 -0300

    cambios

--- sistema_ventas/entidades/tipoproducto.php
+++ sistema_ventas/entidades/tipoproducto.php
@@ -37,4 +37,72 @@ class TipoProducto
         //cerrar el sql
         $mysqli->close();
     }
+
+    public function actualizar()
+    {
+        $mysqli = new mysqli(Config::BBDD_HOST, Config::BBDD_USUARIO, Config::BBDD_CLAVE, Config::BBDD_NOMBRE, Config::BBDD_PORT);
+
+        $sql = "UPDATE tipo_productos SET
+        nombre = '$this->nombre'
+        WHERE idtipoproducto = $this->idtipoproducto";
+
+        if (!$mysqli->query($sql)) {
+            printf("Error en query: %s\n", $mysqli->error . " " . $sql);
+        }
+        $mysqli->close();
+    }
+
+    public function eliminar()
+    {
+        $mysqli = new mysqli(Config::BBDD_HOST, Config::BBDD_USUARIO, Config::BBDD_CLAVE, Config::BBDD_NOMBRE, Config::BBDD_PORT);
+
+        $sql = "DELETE FROM tipo_productos WHERE idtipoproducto = $this->idtipoproducto";
+
+        if (!$mysqli->query($sql)) {
+            printf("Error en query: %s\n", $mysqli->error . " " . $sql);
+        }
+        $mysqli->close();
+    }
+
+    public function obtenerPorId()
+    {
+        $mysqli = new mysqli(Config::BBDD_HOST, Config::BBDD_USUARIO, Config::BBDD_CLAVE, Config::BBDD_NOMBRE, Config::BBDD_PORT);
+        $sql = "SELECT idtipoproducto, 
+                        nombre
+                FROM tipo_productos 
+                WHERE idtipoproducto = " . $this->idtipoproducto;
+        if (!$resultado = $mysqli->query($sql)) {
+            printf("Error en query: %s\n", $mysqli->error . " " . $sql);
+        }
+
+        //Convierte el resultado en un array asociativo
+        if ($fila = $resultado->fetch_assoc()) {
+            $this->nombre = $fila["nombre"];
+        }
+        $mysqli->close();
+    }
+
+    public function obtenerTodos()
+    {
+        $mysqli = new mysqli(Config::BBDD_HOST, Config::BBDD_USUARIO, Config::BBDD_CLAVE, Config::BBDD_NOMBRE, Config::BBDD_PORT);
+        $sql = "SELECT 
+                    idtipoproducto, 
+                    nombre 
+                FROM tipo_productos";
+        if (!$resultado = $mysqli->query($sql)) {
+            printf("Error en query: %s\n", $mysqli->error . " " . $sql);
+        }
+
+        $aResultado = array();
+        if ($resultado) {
+            //Convierte el resultado en un array asociativo
+            while ($fila = $resultado->fetch_assoc()) {
+                $entidadAux = new TipoProducto();
+                $entidadAux->idtipoproducto = $fila["idtipoproducto"];
+                $entidadAux->nombre = $fila["nombre"];
+                $aResultado[] = $entidadAux;
+            }
+        }
+        return $aResultado;
+    }
 }
