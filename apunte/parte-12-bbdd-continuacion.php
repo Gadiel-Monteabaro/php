011ece66e339be863702c1a918df91cd33ab229f
Gadiel-Monteabaro <silvagadiel96@gmail.com>
2022-10-04 12:44:40 -0300

    cambios

--- apunte/parte-12-bbdd-continuacion.php
+++ apunte/parte-12-bbdd-continuacion.php
@@ -7,4 +7,18 @@ Estas funciones nos permiten resolver ciertas preguntas como por ejemplo
 ¿Cual es el importe de las ventas promedio?
 ¿Cual es la cantidad minima de stock de productos?
 ¿Cual fue la facturacion del año 2020?
+
+Las funciones que nos van a permitir resolver esta cuestiones son;
+COUNT() // CANTIDAD
+MIN() // MINIMO 
+MAX() // MAXIMO
+SUM() // SUMAR
+AVG() // PROMEDIO
+*/
+/*
+COUNT ejemplos
+
+SELECT COUNT(*) FROM productos;
+SELECT COUNT(idproducto) FROM productos;
+SELECT COUNT(idproducto) AS cantidad  FROM productos WHERE fk_idtipoproducto = 1;
 */
