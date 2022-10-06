36cc3057f613c7333107eff372de5f1670217c5c
Gadiel-Monteabaro <silvagadiel96@gmail.com>
2022-10-06 14:14:36 -0300

    cambios

--- apunte/parte-3-funciones.php
+++ apunte/parte-3-funciones.php
@@ -23,4 +23,18 @@ $a[1] = 3;
 $a[2] = 5;
 var_dump(count($a)); //Devuelve 3.
 
+//number_format(); -> Formatear un número con los millares agrupados.
+$numero = 1234.56;
+$nombre_format_francais = number_format($número, 2, ',', '.'); // 1.234,56
 
+//date_format(); -> Convierte un string de fecha y lo pasa a formato español.
+$strFecha = "2022-10-06";
+echo date_format(date_create($strFecha), 'd/m/Y'); //06-10-2022
+
+//include_once(); -> La sentencia include_once incluye y evalúa el fichero especificado durante la ejecución del script.
+include_once('header.php');
+
+//Mostrar errores de PHP
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
