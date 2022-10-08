2694768717544e25710d97845cb811766a8a6cee
Gadiel-Monteabaro <silvagadiel96@gmail.com>
2022-10-08 14:13:42 -0300

    cambios

--- apunte/php_embebido.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*
-PHP-Embebido => permite mejorar nuestro codigo de manera mas accesible, de tal forma que sea mas comodo de escribir y de visualizar.
-*/
-foreach ($aNombres as $nombre) : ?>
-    hola <?php echo $nombre; ?> <br>
-<?php endforeach; ?>
